793c2c8e7e75942329ac30ce7ad03970a9826ca3
Janix4000 <janix4000@gmail.com>
2021-03-15 22:53:50 +0100

    Added rest of functions in mergee

--- lab1/lib_merge.c
+++ lab1/lib_merge.c
@@ -1,8 +1,10 @@
 #include "lib_merge.h"
+#include <string.h>
+#include <stdio.h>
 
 int test_function()
 {
-    return 1;
+    return 1; //
 }
 
 size_t libReadLineBlock(LibLinesBlocks *blocks, FILE *mergedLinesFile)
@@ -17,12 +19,17 @@ size_t libReadLineBlock(LibLinesBlocks *blocks, FILE *mergedLinesFile)
         line = NULL;
         nLine = 0;
     }
+    free(line);
     vecPushBack(blocks, lines);
     return 1;
 }
 
-void _libFreeLines(LibLines *lines)
+static void _libFreeLines(LibLines *lines)
 {
+    while (lines->size)
+    {
+        free(vecPopBack(lines));
+    }
     vecFree(lines);
     free(lines);
 }
@@ -43,15 +50,93 @@ void libRemoveLineInAt(LibLinesBlocks *blocks, size_t blockIdx, size_t lineIdx)
     }
 }
 
-void printLinesBlocks(LibLinesBlocks *blocks)
+void libPrintLinesBlocks(LibLinesBlocks *blocks)
 {
     for (size_t iBlock = 0; iBlock < blocks->size; iBlock++)
     {
         LibLines *lines = blocks->container[iBlock];
         for (size_t iLine = 0; iLine < lines->size; iLine++)
         {
-            printf("%s\n", lines->container[iLine]);
+            printf("%s\n", (char *)lines->container[iLine]);
         }
         printf("\n");
     }
+}
+
+void libFreeRowBlocks(LibLinesBlocks *blocks)
+{
+    while (blocks->size)
+    {
+        _libFreeLines(vecPopBack(blocks));
+    }
+    free(blocks);
+}
+
+void libAddFilenamePair(LibFilenamePairs *pairs, const char *filenamePair)
+{
+    FilenamePair *pair = malloc(sizeof *pair);
+    char *colonPos = strchr(filenamePair, ':');
+    pair->firstFilename = strndup(filenamePair, colonPos - filenamePair);
+    pair->secondFilename = strdup(colonPos + 1);
+    vecPushBack(pairs, pair);
+}
+
+static void _libMergePairOfFilesToTmp(FILE *firstFile, FILE *secondFile, FILE *tmp)
+{
+    char *fLine = NULL, *sLine = NULL;
+    size_t nFirst, nSecond;
+    while (
+        getline(&fLine, &nFirst, firstFile) != 1 &&
+        getline(&sLine, &nSecond, secondFile) != 1)
+    {
+        fwrite(fLine, sizeof *fLine, nFirst, tmp);
+        fwrite(sLine, sizeof *sLine, nSecond, tmp);
+    }
+    rewind(tmp);
+    free(fLine);
+    free(sLine);
+}
+
+void libMergeFilePairs(LibFiles *tmpFiles, LibFilenamePairs *filenamePairs)
+{
+    for (size_t iPair = 0; iPair < filenamePairs->size; iPair++)
+    {
+        FilenamePair *pair = filenamePairs->container[iPair];
+        FILE *tmp = tmpfile();
+        FILE *firstFile = fopen(pair->firstFilename, "r");
+        FILE *secondFile = fopen(pair->secondFilename, "r");
+        if (tmp && firstFile && secondFile)
+        {
+            _libMergePairOfFilesToTmp(firstFile, secondFile, tmp);
+            vecPushBack(tmpFiles, tmp);
+        }
+        else
+        {
+            fclose(tmp);
+        }
+        fclose(firstFile);
+        fclose(secondFile);
+    }
+}
+
+void libFreeFilePairs(LibFilenamePairs *filenamePairs)
+{
+    while (filenamePairs->size)
+    {
+        FilenamePair *pair = vecPopBack(filenamePairs);
+        free(pair->firstFilename);
+        free(pair->secondFilename);
+        free(pair);
+    }
+    free(filenamePairs);
+}
+
+void libFreeFiles(LibFiles *tmpFiles)
+{
+    while (tmpFiles->size)
+    {
+        FILE *tmp = vecPopBack(tmpFiles);
+        fclose(tmp);
+    }
+    free(tmpFiles);
 }
--- lab1/lib_merge.h
+++ lab1/lib_merge.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <stdlib.h>
-#include <stdio.h>
 #include "lib_vector.h"
 
 typedef struct
@@ -19,12 +18,12 @@ int test_function();
 
 size_t libReadLineBlock(LibLinesBlocks *blocks, FILE *mergedLinesFile);
 void libRemoveLineBlockAt(LibLinesBlocks *blocks, size_t idx);
-void _libFreeLines(LibLines *lines) void removeLineInAt(LibLinesBlocks *blocks, size_t blockIdx, size_t lineIdx);
-void printLinesBlocks(LibLinesBlocks *blocks);
-void freeRowBlocks(LibLinesBlocks *blocks);
+void libRemoveLineInAt(LibLinesBlocks *blocks, size_t blockIdx, size_t lineIdx);
+void libPrintLinesBlocks(LibLinesBlocks *blocks);
+void libFreeRowBlocks(LibLinesBlocks *blocks);
 
-void addFilenamePair(LibFilenamePairs *pairs, const char *firstFilename, const char *secondFilename);
-void mergeFilePairs(LibFiles *tmpFiles, LibFilenamePairs *filenamePairs);
-void freeFilePairs(LibFilenamePairs *filenamePairs);
+void libAddFilenamePair(LibFilenamePairs *pairs, const char *filenamePair);
+void libMergeFilePairs(LibFiles *tmpFiles, LibFilenamePairs *filenamePairs);
+void libFreeFilePairs(LibFilenamePairs *filenamePairs);
 
-void freeFiles(LibFiles *tmpFiles);
+void libFreeFiles(LibFiles *tmpFiles);
