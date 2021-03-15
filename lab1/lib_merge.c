d5c650d6602fad14571641dc39aafb7eb2ef0dab
Janix4000 <janix4000@gmail.com>
2021-03-15 20:16:34 +0100

    Added read from tmp, removing and printing

--- lab1/lib_merge.c
+++ lab1/lib_merge.c
@@ -4,3 +4,54 @@ int test_function()
 {
     return 1;
 }
+
+size_t libReadLineBlock(LibLinesBlocks *blocks, FILE *mergedLinesFile)
+{
+    char *line = NULL;
+    size_t nLine = 0;
+    LibLines *lines = malloc(sizeof *lines);
+    vecInit(lines);
+    while (getline(&line, &nLine, mergedLinesFile) != -1)
+    {
+        vecPushBack(lines, line);
+        line = NULL;
+        nLine = 0;
+    }
+    vecPushBack(blocks, lines);
+    return 1;
+}
+
+void _libFreeLines(LibLines *lines)
+{
+    vecFree(lines);
+    free(lines);
+}
+
+void libRemoveLineBlockAt(LibLinesBlocks *blocks, size_t idx)
+{
+    LibLines *lines = vecEraseAt(blocks, idx);
+    _libFreeLines(lines);
+}
+
+void libRemoveLineInAt(LibLinesBlocks *blocks, size_t blockIdx, size_t lineIdx)
+{
+    LibLines *lines = blocks->container[blockIdx];
+    free(vecEraseAt(lines, lineIdx));
+    if (lines->size == 0)
+    {
+        libRemoveLineBlockAt(blocks, blockIdx);
+    }
+}
+
+void printLinesBlocks(LibLinesBlocks *blocks)
+{
+    for (size_t iBlock = 0; iBlock < blocks->size; iBlock++)
+    {
+        LibLines *lines = blocks->container[iBlock];
+        for (size_t iLine = 0; iLine < lines->size; iLine++)
+        {
+            printf("%s\n", lines->container[iLine]);
+        }
+        printf("\n");
+    }
+}
