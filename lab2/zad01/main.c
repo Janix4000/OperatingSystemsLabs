aaf547af743cd7a225413a7ed39534af9dd9cae5
Janix4000 <janix4000@gmail.com>
2021-03-18 19:08:58 +0100

    Splited to lib

--- lab2/zad01/main.c
+++ lab2/zad01/main.c
@@ -4,90 +4,12 @@
 #include <stdbool.h>
 #include <string.h>
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include "lib_uni_file.h"
 
 #define LIB_EMPTY 0
 #define LIB_SYS 1
 #define LIB_C 2
 #define LIB_ERR -1
-
-typedef struct
-{
-    union
-    {
-        FILE *cfile;
-        int sysid;
-    } file;
-    char type;
-} LibUniFile;
-
-int libOpen(const char *fileName, LibUniFile *uniFile, char type)
-{
-    if (type == LIB_SYS)
-    {
-        int id = open(fileName, O_RDWR);
-        if (id < 0)
-        {
-            goto couldnt_open;
-        }
-        uniFile->file.sysid = id;
-    }
-    else
-    {
-        FILE *file = fopen(fileName, "r+");
-        if (!file)
-        {
-            goto couldnt_open;
-        }
-        uniFile->file.cfile = file;
-    }
-    uniFile->type = type;
-    return 1;
-couldnt_open:
-    uniFile->type = LIB_ERR;
-    fprintf(stderr, "Couldnt open file \"%s\".\n", fileName);
-    return 0;
-}
-
-void libClose(LibUniFile *uniFile)
-{
-    if (uniFile->type == LIB_C)
-    {
-        fclose(uniFile->file.cfile);
-    }
-    else if (uniFile->type == LIB_SYS)
-    {
-        close(uniFile->file.sysid);
-    }
-}
-
-size_t libRead(void *buffor, size_t size, size_t count, LibUniFile *file)
-{
-    if (file->type == LIB_C)
-    {
-        return fread(buffor, size, count, file->file.cfile);
-    }
-    else if (file->type == LIB_SYS)
-    {
-        return read(file->file.sysid, buffor, size * count);
-    }
-    return 0;
-}
-
-size_t libWrite(void *buffor, size_t size, size_t count, LibUniFile *file)
-{
-    if (file->type == LIB_C)
-    {
-        return fwrite(buffor, size, count, file->file.cfile);
-    }
-    else if (file->type == LIB_SYS)
-    {
-        return write(file->file.sysid, buffor, size * count);
-    }
-    return 0;
-}
 
 int main(int argc, char **argv)
 {
