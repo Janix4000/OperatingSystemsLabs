29284b4b3af0cfcd3b31da7da4007aa60848fc8a
janix4000 <janix4000@gmail.com>
2021-05-10 23:50:14 +0200

    Made pgm save

--- lab08/main.c
+++ lab08/main.c
@@ -3,7 +3,7 @@
 
 typedef struct
 {
-    char *img;
+    unsigned char *img;
     int width;
     int height;
 } Image;
@@ -11,6 +11,34 @@ typedef struct
 void free_image(Image *img)
 {
     free(img->img);
+}
+
+int save_pgm(const char *filename, const Image *img)
+{
+    FILE *file = fopen(filename, "w");
+    if (file == NULL)
+    {
+        perror("fopen");
+        return -1;
+    }
+    const char pred[] = "P2\n";
+    fputs(pred, file);
+    const int max_col = 255;
+    fprintf(file, "%d %d\n%d\n", (int)img->width, (int)img->height, max_col);
+    for (size_t y = 0; y < img->height; y++)
+    {
+        for (size_t x = 0; x < img->width; x++)
+        {
+            int tmp = (int)img->img[x + y * img->height];
+            fprintf(file, "%d ", tmp);
+        }
+        fprintf(file, "\n");
+    }
+    return 0;
+}
+
+void negate(const Image *src, Image *dest)
+{
 }
 
 int load_pgm(const char *filename, Image *img)
@@ -32,10 +60,10 @@ int load_pgm(const char *filename, Image *img)
         {
             int tmp;
             fscanf(file, "%d", &tmp);
-            img->img[x + y * img->height] = (char)tmp;
-            printf("%d ", tmp);
+            img->img[x + y * img->height] = (unsigned char)tmp;
+            // printf("%d ", tmp);
         }
-        printf("\n");
+        // printf("\n");
     }
     return 0;
 }
@@ -44,6 +72,7 @@ int main(int argc, char const *argv[])
 {
     Image img;
     load_pgm("file.pgm", &img);
+    save_pgm("file2.pgm", &img);
     free_image(&img);
     return 0;
 }
